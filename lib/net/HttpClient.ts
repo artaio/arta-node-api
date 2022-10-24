928c749be5eefd797f2100ac340720fb347c32da
Otávio Jacobi <otaviojacobi@gmail.com>
2022-10-24 13:08:38 -0300

    chore: fix eslint with prettier and remove tests from dist

--- lib/net/HttpClient.ts
+++ lib/net/HttpClient.ts
@@ -1,26 +1,25 @@
-type HttpMethod = 'get'
-| 'post'
-| 'patch'
-| 'put'
-| 'delete';
+export type HttpMethod = 'get' | 'post' | 'patch' | 'put' | 'delete';
 
 export interface HttpClientResponse {
-  statusCode?: number
-  headers?: any
-  body: () => Promise<string>
-  json: () => Promise<any>
+  statusCode?: number;
+  headers?: any;
+  body: () => Promise<string>;
+  json: () => Promise<any>;
 }
 
 export interface HttpRequestParameters {
-  protocol: 'http' | 'https'
-  port: number
-  path: string
-  method: HttpMethod
-  headers: any
-  requestData: string
-  timeout: number
+  protocol: 'http' | 'https';
+  port: number;
+  path: string;
+  method: HttpMethod;
+  headers: any;
+  requestData: string;
+  timeout: number;
 }
 
 export interface HttpClient {
-  request: (host: string, params?: Partial<HttpRequestParameters>) => Promise<HttpClientResponse>
+  request: (
+    host: string,
+    params?: Partial<HttpRequestParameters>
+  ) => Promise<HttpClientResponse>;
 }

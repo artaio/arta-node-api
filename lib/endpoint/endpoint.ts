5041b4681bb25b4c5e5711b005cc506e5b843cd4
Otávio Jacobi <otaviojacobi@gmail.com>
2022-10-31 12:44:58 -0300

    feat: adds paginated query to webhooks

--- lib/endpoint/endpoint.ts
+++ lib/endpoint/endpoint.ts
@@ -1,9 +1,15 @@
 import { ArtaID } from '../ArtaClient';
 import { RestClient } from '../net/RestClient';
 import { convertDatesToUtc, DatedInterface } from '../utils';
+import { PageMetada } from '../pagination';
 
 export interface Endpoint<T, U> {
-  list: (page?: number, pageSize?: number, auth?: string) => Promise<T[]>;
+  list: (
+    page?: number,
+    pageSize?: number,
+    auth?: string
+  ) => Promise<{ items: T[]; metadata: PageMetada }>;
+  listAll: (auth?: string, onReturn?: (params: any) => T) => AsyncGenerator<T>;
   getById: (id: ArtaID, auth?: string) => Promise<T>;
   create: (payload: U, auth?: string) => Promise<T>;
   update: (
@@ -27,12 +33,36 @@ export class DefaultEndpoint<T extends DatedInterface, U>
     return convertDatesToUtc(req) as T;
   }
 
-  public async list(page = 1, pageSize = 20, auth?: string): Promise<T[]> {
+  public async list(
+    page = 1,
+    pageSize = 20,
+    auth?: string
+  ): Promise<{ items: T[]; metadata: PageMetada }> {
     const body = await this.artaClient.get(
       `${this.path}?page=${page}&page_size=${pageSize}`,
       auth
     );
-    return body.items.map((item: T) => convertDatesToUtc(item));
+    const items: T[] = body.items.map((item: T) => convertDatesToUtc(item));
+
+    return { items, metadata: body.metadata };
+  }
+
+  public async *listAll(
+    auth?: string,
+    onReturn?: (params: any) => T
+  ): AsyncGenerator<T> {
+    let page = 1;
+    let returned_elements = 0;
+    let body;
+    do {
+      body = await this.artaClient.get(`${this.path}?page=${page}`, auth);
+      page = body.metadata.page + 1;
+      for (const item of body.items) {
+        const withUtc = convertDatesToUtc(item);
+        returned_elements++;
+        yield onReturn ? onReturn(withUtc) : (withUtc as T);
+      }
+    } while (body.metadata.total_count > returned_elements);
   }
 
   public async create(payload: U, auth?: string): Promise<T> {

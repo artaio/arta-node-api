c67476cd3f2404c2e4aab75207fce2a20ef09ddf
Dylan Fareed <email@dylanfareed.com>
2022-11-15 12:58:32 -0500

    refactor: moves onReturn to DefaultEndpoint constructor; updates payments and webhooks

--- lib/endpoint/endpoint.ts
+++ lib/endpoint/endpoint.ts
@@ -4,28 +4,14 @@ import { convertDatesToUtc, DatedInterface } from '../utils';
 import { Page } from '../pagination';
 
 export interface Endpoint<T, U> {
-  list: (
-    page?: number,
-    pageSize?: number,
-    auth?: string,
-    onReturn?: (params: any) => T
-  ) => Promise<Page<T>>;
+  list: (page?: number, pageSize?: number, auth?: string) => Promise<Page<T>>;
   listAll: (auth?: string, onReturn?: (params: any) => T) => AsyncGenerator<T>;
-  getById: (
-    id: ArtaID,
-    auth?: string,
-    onReturn?: (params: any) => T
-  ) => Promise<T>;
-  create: (
-    payload: U,
-    auth?: string,
-    onReturn?: (params: any) => T
-  ) => Promise<T>;
+  getById: (id: ArtaID, auth?: string) => Promise<T>;
+  create: (payload: U, auth?: string) => Promise<T>;
   update: (
     id: ArtaID,
     payload: Partial<U> | Partial<T>,
-    auth?: string,
-    onReturn?: (params: any) => T
+    auth?: string
   ) => Promise<T>;
   remove: (id: ArtaID, auth?: string) => Promise<void>;
 }
@@ -33,41 +19,35 @@ export interface Endpoint<T, U> {
 export class DefaultEndpoint<T extends DatedInterface, U>
   implements Endpoint<T, U>
 {
+  private readonly onReturn?: (params: any) => T;
   private readonly path: string;
-  constructor(path: string, private readonly artaClient: RestClient) {
+  constructor(
+    path: string,
+    private readonly artaClient: RestClient,
+    onReturn?: (params: any) => T
+  ) {
+    this.onReturn = onReturn;
     this.path = path.startsWith('/') ? path : `/${path}`;
   }
 
-  public async getById(
-    id: ArtaID,
-    auth?: string,
-    onReturn?: (params: any) => T
-  ): Promise<T> {
+  public async getById(id: ArtaID, auth?: string): Promise<T> {
     const req = await this.artaClient.get(`${this.path}/${id}`, auth);
-    return this.processBody(req, onReturn);
+    return this.processBody(req);
   }
 
-  public async list(
-    page = 1,
-    pageSize = 20,
-    auth?: string,
-    onReturn?: (params: any) => T
-  ): Promise<Page<T>> {
+  public async list(page = 1, pageSize = 20, auth?: string): Promise<Page<T>> {
     const body = await this.artaClient.get(
       `${this.path}?page=${page}&page_size=${pageSize}`,
       auth
     );
     const items: T[] = body.items.map((item: T) => {
-      return this.processBody(item, onReturn);
+      return this.processBody(item);
     });
 
     return { items, metadata: body.metadata };
   }
 
-  public async *listAll(
-    auth?: string,
-    onReturn?: (params: any) => T
-  ): AsyncGenerator<T> {
+  public async *listAll(auth?: string): AsyncGenerator<T> {
     let page = 1;
     let returned_elements = 0;
     let body;
@@ -76,44 +56,36 @@ export class DefaultEndpoint<T extends DatedInterface, U>
       page = body.metadata.page + 1;
       for (const item of body.items) {
         returned_elements++;
-        yield this.processBody(item, onReturn);
+        yield this.processBody(item);
       }
     } while (body.metadata.total_count > returned_elements);
   }
 
-  public async create(
-    payload: U,
-    auth?: string,
-    onReturn?: (params: any) => T
-  ): Promise<T> {
+  public async create(payload: U, auth?: string): Promise<T> {
     const body = await this.artaClient.post(this.path, payload, auth);
-    return this.processBody(body, onReturn);
+    return this.processBody(body);
   }
 
   public async update(
     id: ArtaID,
     payload: Partial<U> | Partial<T>,
-    auth?: string,
-    onReturn?: (params: any) => T
+    auth?: string
   ): Promise<T> {
     const body = await this.artaClient.patch(
       `${this.path}/${id}`,
       payload,
       auth
     );
-    return this.processBody(body, onReturn);
+    return this.processBody(body);
   }
 
   public async remove(id: ArtaID, auth?: string): Promise<void> {
     return await this.artaClient.delete(`${this.path}/${id}`, auth);
   }
 
-  private processBody(
-    payload: Partial<U> | Partial<T>,
-    onReturn?: (params: any) => T
-  ): T {
+  private processBody(payload: Partial<U> | Partial<T>): T {
     let resource = convertDatesToUtc(payload);
-    resource = onReturn ? onReturn(resource) : resource;
+    resource = this.onReturn ? this.onReturn(resource) : resource;
     return resource as T;
   }
 }
--- lib/endpoint/payments.ts
+++ lib/endpoint/payments.ts
@@ -17,20 +17,20 @@ export interface Payments extends DatedInterface {
 export class PaymentsEndpoint {
   private readonly defaultEndpoint: Endpoint<Payments, never>;
   private readonly path = '/payments';
-
   constructor(private readonly artaClient: RestClient) {
     this.defaultEndpoint = new DefaultEndpoint<Payments, never>(
       this.path,
-      this.artaClient
+      this.artaClient,
+      this.enrichFields
     );
   }
 
   public getById(id: ArtaID, auth?: string): Promise<Payments> {
-    return this.defaultEndpoint.getById(id, auth, this.enrichFields);
+    return this.defaultEndpoint.getById(id, auth);
   }
 
   public list(page = 1, pageSize = 20, auth?: string): Promise<Page<Payments>> {
-    return this.defaultEndpoint.list(page, pageSize, auth, this.enrichFields);
+    return this.defaultEndpoint.list(page, pageSize, auth);
   }
 
   private enrichFields(resource: Payments): Payments {

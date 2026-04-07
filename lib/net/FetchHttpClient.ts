9ca3f5fdbb17efb2936735d6591282afd6e7fc66
Otavio Jacobi <otaviojacobi@gmail.com>
2026-04-07 10:59:37 -0300

    chore(deps): upgrade eslint-plugin-promise to v7
    Bump eslint-plugin-promise from v6 to v7 and activate it by extending
    plugin:promise/recommended.
    
    - Fix promise/param-names: rename unused resolve parameter from _ to
      _resolve in FetchHttpClient timeout promise

--- lib/net/FetchHttpClient.ts
+++ lib/net/FetchHttpClient.ts
@@ -67,7 +67,7 @@ export class FetchHttpClient implements HttpClient {
     });
 
     let setTimeoutId: ReturnType<typeof setTimeout> | null = null;
-    const rejectTimeout = new Promise((_, reject) => {
+    const rejectTimeout = new Promise((_resolve, reject) => {
       setTimeoutId = setTimeout(() => {
         reject(new Error(`Request timed out to ${host}:${port}`));
       }, timeout);
